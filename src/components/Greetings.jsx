9f4017d512c1710c064386b23fb6328383d6ade3
Gaurav Bhardwaj <gauravbhardwaj0025@gmail.com>
2025-09-06 01:13:24 +0530

    add new component greetings.jsx and set dynamically name

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Greetings() {
+
+
+    const [name, setName] = useState("");
+    return (
+        <>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Enter Your Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                >
+                </input>
+
+
+                <h2 style={{ marginTop: "20px", color: "#2c3e50" }}>
+                    Hello, {name}!
+                </h2>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Greetings
